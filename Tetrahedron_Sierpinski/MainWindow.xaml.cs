8d679ebe45cb84c7f119fd34d9b5cca564118fa5
RJank <rjankevic1@gmail.com>
2026-03-29 16:34:27 +0300

    GUI changes + comments
    Changed some text , no code changes , clean-up of libraries used

--- Tetrahedron_Sierpinski/MainWindow.xaml.cs
+++ Tetrahedron_Sierpinski/MainWindow.xaml.cs
@@ -1,24 +1,14 @@
-﻿using System.Diagnostics;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Tetrahedron_Sierpinski
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // deklaracja zmiennych
         Model3DGroup group = new Model3DGroup();
         DispatcherTimer timer;
 
@@ -29,26 +19,26 @@ namespace Tetrahedron_Sierpinski
         bool autoRotate = true;
         double distance = 20;
         int autoBuildCounter = 0;
-
         Point lastMouse;
         bool isDragging = false;
 
         public MainWindow()
         {
             InitializeComponent();
-
+            // ustawienia kamery i światła
             Transform3DGroup transformGroup = new Transform3DGroup();
             transformGroup.Children.Add(new RotateTransform3D(axisRotationX));
             transformGroup.Children.Add(new RotateTransform3D(axisRotationY));
+            // dodanie modelu do viewportu
             group.Transform = transformGroup;
-
             ModelVisual3D modelVisual = new ModelVisual3D { Content = group };
             viewport.Children.Add(modelVisual);
-
+            // inicjalizacja modelu i animacji
             RefreshModel(0);
             StartAnimation();
         }
 
+        // metoda uruchamiająca animację
         void StartAnimation()
         {
             timer = new DispatcherTimer();
@@ -56,15 +46,16 @@ namespace Tetrahedron_Sierpinski
             timer.Tick += Timer_Tick;
             timer.Start();
         }
-
+        // metoda obsługująca zdarzenie tick timera
         void Timer_Tick(object sender, EventArgs e)
         {
+            // automatyczna rotacja
             if (autoRotate)
             {
                 axisRotationY.Angle = (axisRotationY.Angle + 1.2) % 360;
                 axisRotationX.Angle = (axisRotationX.Angle + 0.6) % 360;
             }
-
+            // automatyczne budowanie poziomów
             if (isAuto)
             {
                 autoBuildCounter++;
@@ -82,12 +73,12 @@ namespace Tetrahedron_Sierpinski
                 }
             }
         }
-
+        // metoda obsługująca zmianę wartości slider'a poziomu
         void LevelSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
                 RefreshModel((int)e.NewValue);
         }
-
+        // metoda odświeżająca model na podstawie wybranego poziomu
         void RefreshModel(int lvl)
         {
             group.Children.Clear();
@@ -97,64 +88,73 @@ namespace Tetrahedron_Sierpinski
             double size = 5;
             DrawSierpinski(lvl, new Point3D(-size / 2, -size / 2, -size / 2), size, lvl);
         }
-
+        // metoda rekurencyjna rysująca fraktal Sierpińskiego
         void DrawSierpinski(int lvl, Point3D pos, double size, int totalDepth)
         {
+            // jeśli poziom głębokości jest równy 0, dodaj pojedynczy tetrahedron
             if (lvl == 0)
             {
                 AddTetrahedron(pos, size, totalDepth);
                 return;
             }
-
+            // obliczanie połowy rozmiaru i wywoływanie rekurencyjnie dla 4 mniejszych tetrahedronów
             double half = size / 2;
             DrawSierpinski(lvl - 1, pos, half, totalDepth);
             DrawSierpinski(lvl - 1, new Point3D(pos.X + half, pos.Y, pos.Z), half, totalDepth);
             DrawSierpinski(lvl - 1, new Point3D(pos.X + half / 2, pos.Y + half, pos.Z), half, totalDepth);
             DrawSierpinski(lvl - 1, new Point3D(pos.X + half / 2, pos.Y + half / 2, pos.Z + half), half, totalDepth);
         }
-
+        // metoda dodająca pojedynczy tetrahedron do modelu
         void AddTetrahedron(Point3D p, double s, int depth)
         {
+            // tworzenie siatki tetrahedronu
             MeshGeometry3D mesh = new MeshGeometry3D();
+            // wierzchołki tetrahedronu
             Point3D p0 = p;
             Point3D p1 = new Point3D(p.X + s, p.Y, p.Z);
             Point3D p2 = new Point3D(p.X + s / 2, p.Y + s, p.Z);
             Point3D p3 = new Point3D(p.X + s / 2, p.Y + s / 2, p.Z + s);
-
+            // dodawanie wierzchołków do siatki
             mesh.Positions.Add(p0);
             mesh.Positions.Add(p1);
             mesh.Positions.Add(p2);
             mesh.Positions.Add(p3);
-
+            // indeksy trójkątów tworzących tetrahedron
             int[] indices = {
                 0, 1, 2,
                 0, 1, 3,
                 1, 2, 3,
                 0, 2, 3 
             };
-
+            // kolorowanie tetrahedronu na podstawie poziomu głębokości
             Color color = Color.FromRgb(
                 (byte)(50 + depth * 30),
                 (byte)(100),
                 (byte)(200 - depth * 30)
             );
+            // dodawanie indeksów do siatki
             foreach (int i in indices) mesh.TriangleIndices.Add(i);
+            // tworzenie modelu 3D z siatki i materiału
             var model = new GeometryModel3D(mesh, new DiffuseMaterial(new SolidColorBrush(color)));
+            // ustawienie materiału tylnej ściany na ten sam kolor
             model.BackMaterial = model.Material;
+            // dodawanie modelu do grupy
             group.Children.Add(model);
         }
-
+        // metoda obsługująca kliknięcie przycisku ustawienia maksymalnego poziomu
         void SetMax_Click(object sender, RoutedEventArgs e)
         {
+            // próba parsowania wartości z TextBox'a i ustawienie maksymalnego poziomu slider'a
             if (int.TryParse(maxLevelTextBox.Text, out int newMax))
             {
+                // ograniczenie maksymalnego poziomu do 7 ze względu na wydajność
                 if (newMax > 7) newMax = 7; 
                 levelSlider.Maximum = newMax;
                 maxLevelTextBox.Text = newMax.ToString();
-                MessageBox.Show("Maksymalny poziom ustawiony na: " + newMax);
+                //MessageBox.Show("Maksymalny poziom ustawiony na: " + newMax);
             }
         }
-
+        // metoda obsługująca kliknięcie przycisku startującego automatyczne budowanie poziomów
         void Start_Click(object sender, RoutedEventArgs e)
         {
             levelSlider.Value = 0;
@@ -162,20 +162,19 @@ namespace Tetrahedron_Sierpinski
             isAuto = true;
             autoRotate = true;
         }
-
+        // metoda obsługująca kliknięcie przycisku zatrzymującego automatyczną rotację i budowanie poziomów
         void Stop_Click(object sender, RoutedEventArgs e)
         {
             isAuto = false;
             autoRotate = false;
         }
-
+        // metody obsługujące zdarzenia myszy do ręcznej rotacji modelu
         void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed) {
                 isDragging = true; lastMouse = e.GetPosition(this); 
             }
         }
-
         void Window_MouseMove(object sender, MouseEventArgs e)
         {
             if (!isDragging) return;
@@ -184,12 +183,11 @@ namespace Tetrahedron_Sierpinski
             axisRotationX.Angle += (current.Y - lastMouse.Y) * 0.5;
             lastMouse = current;
         }
-
         void Window_MouseUp(object sender, MouseButtonEventArgs e)
         {
             isDragging = false;
         }
-
+        // metoda obsługująca zdarzenie mousescroll do przybliżania i oddalania kamery
         void Window_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             distance -= e.Delta * 0.01;
